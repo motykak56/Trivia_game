dafc9b8717a038edb2848bd917e1ba1893043717
motykak <motykak@outlook.com>
2019-06-21 11:00:55 -0500

    This is the work so far on the homework 5 assignment.

---
+++ Assets/javascript/app.js
@@ -0,0 +1,18 @@
+
+var rightAnswers= [Kennedy, Reagan, Harding, Adams, Washington, JAdams, 
+    VanBuren, Buchanan, T. Roosevelt, Lincoln]
+
+var choiceBox= [input]
+
+var question= (question_number)
+
+var indexnum=(question)
+
+var function run loop (){
+    if 
+    (indexnum.choiceBox === indexnum.rightAnswers)
+    move to successAnswer.html
+    else
+    move to failAnswer.html
+}
+
