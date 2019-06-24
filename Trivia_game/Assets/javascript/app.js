2a82b95f885d64babf669cdd08687d4315b75f9c
Kathleen Motyka <motykak@outlook.com>
2019-06-24 02:04:40 -0500

    Add files via upload
    I've spent about 15 hours on this over the past day.  It still won't run.  I'm simply not that slow of a person mentally.  I can see what needs to be done....at least I think so.  I've got a session with my tutor on Tuesday evening and I'm going to pepper her with questions.  I should have it ironed out after that.  I'd hoped to get my other homework done this weekend but I spent too much time on this one.  If I don't have my javascript down pat, I don't see how the rest is going to go well either.

---
+++ Trivia_game/Assets/javascript/app.js
@@ -0,0 +1,72 @@
+
+/*var rightAnswers=["Kennedy", "Reagan", "Harding", "Adams", "Washington", "JAdams", 
+    "VanBuren", "Buchanan", "T. Roosevelt", "Lincoln"] don't need array of right answers
+    if right answer is defined on each question page*/
+
+var rightAnswer=document.getElementById("rightAnswer");{
+    console.log=rightAnswer};
+
+var q=document.getElementById("question_number");{
+console.log=q};
+
+var selPresident=[]
+
+var a=(0);
+//holding the sum of right answers//
+
+var b=(0);
+//holding the sum of wrong answers//
+
+function move (){
+document.getElementById("nextStop")(href, "nextStop");
+(console.log=move)};
+//moves user to next page//
+
+function displayResults(selPresident) {
+    var displayResults = displayResults;{
+    let x="rightAnswer";
+    let y="president";
+    if
+    (x==y){
+        a = (a+1)
+        result = "You got the right answer!!"}
+    else{
+        b = (b+1)
+        result = "Sorry, that's the wrong answer."};
+
+var p=document.getElementById("selPresident");{
+            console.log=p};
+    
+addEventListener(type="click", listener="click", p)
+var president=document.getSelection("p");{
+    (console.log=president)};
+
+F(type="text")();
+
+move(type="url")()};
+        
+        /*function location(href="move"){
+        console.log=move}; */
+
+    /*let y=(-1);
+    let z=x+y;
+    if 
+    (z==indexOf("rightAnswers")=="selPresident"){
+        succ = (succ+1);
+        result = "You got the right answer!!"; 
+            } 
+    else { 
+        fail = (fail+1); 
+        result = "Sorry, that's the wrong answer."; 
+            }
+        };
+var location=Location.href="move";{
+console.log=move};
+
+/*var answer=(((tabindex)).indexOf(rightAnswers));{
+console.log=answer;}*/
+
+/*var locHtml=((tabindex).indexOf(move))
+console.log=locHtml;*/
+
+
